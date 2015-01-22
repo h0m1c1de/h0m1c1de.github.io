ce4cd13af6f9808c87c26f9822ed4fed1c8a115a
Taras Dihtenko <h0m1c1de@mail.ru>
2015-01-23 00:46:46 +0200

    scrollTo added

--- iddo/default.js
+++ iddo/default.js
@@ -1,8 +1,31 @@
-$(document).ready(function(){
-	$(".work").hover( function(){
+$(document).ready(function () {
+	$(".work").hover(function () {
 		$(this).find(".designedby").stop().slideToggle(".hiden");
 	});
-	$(".skills a").on("click", function(){
-		$(this).find(".glyphicon-plus, .glyphicon-minus").toggleClass('hide');
+	var $panel = $(".panel");
+	$(".skills a").on("click", function () {
+		if($(this).closest(".panel").hasClass("opened")){
+			$(this).closest(".panel").removeClass('opened');
+		}else{
+			
+		$panel.removeClass('opened');
+		$(this).closest(".panel").addClass('opened');
+		}
+		
+
 	});
+
+	function scrollTo(buttonClass, scrollToClass) {
+		$(buttonClass).click(function () {
+			$('html, body').animate({
+				scrollTop: $(scrollToClass).position().top
+			}, 1000);
+		});
+
+	}
+	scrollTo(".serv", ".services");
+	scrollTo(".port", ".works");
+	scrollTo(".abo", ".team");
+	scrollTo(".cont", ".contactus");
+
 });
